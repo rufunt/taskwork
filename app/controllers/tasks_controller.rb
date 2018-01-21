3e1c848b471f20708d3abd112fdb1747e3a12325
rufunt <jazzoo@mail.ru>
2018-01-22 01:18:01 +0300

    Add Tasks actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,38 @@ class TasksController < ApplicationController
   def index
     
   end
+
+  def show
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def task_params
+    
+  end
+
+  def find_task
+    
+  end
 end
