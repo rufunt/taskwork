5a4dff21394c7cf9db22f25802bd037569f8ce32
rufunt <jazzoo@mail.ru>
2018-01-23 22:59:57 +0300

    Navigation and Sorting Tasks by Categories

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,12 @@ class TasksController < ApplicationController
   before_action :find_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.all.order("created_at DESC")
+    if params[:category].blank?
+      @tasks = Task.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @tasks = Task.where(category_id: @category_id).order("created_at DESC")
+    end
   end
 
   def show
